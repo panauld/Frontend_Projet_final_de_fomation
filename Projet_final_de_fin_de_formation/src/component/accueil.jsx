e957d0e4b7389b2a3d7c04cc8416850baac0cc78
ColexGod <ccfelix2004@gmail.com>
2024-01-17 09:21:08 -0500

    accueil.jsx

--- Projet_final_de_fin_de_formation/src/component/accueil.jsx
+++ Projet_final_de_fin_de_formation/src/component/accueil.jsx
@@ -1,6 +1,102 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import "../../global.css";
 
 const Accueil = ({}) => {
-  return <body></body>;
+  return;
+  <div class="accueilbody">
+    <div class="imagebasecontainer">
+      <h2>Unbelievable</h2>
+      <h4>Quality.Comfort.</h4>
+      <div class="div1">
+        <img class="img1" src="../img/BaseShirt.jpg" />
+      </div>
+      <div class="div2">
+        <img class="img1" src="../img/BasePants.jpg" />
+      </div>
+      <div class="div3">
+        <img class="img3" src="../img/BaseShoes.jpg" />
+      </div>
+    </div>
+    <div class="lineargrad">
+      <div class="popularcontainer">
+        <h3 class="ventespop">Ventes populaires</h3>
+        <div class="popular">
+          <div class="pop1">
+            <img class="imgpopular" src="../img/PhotoPopular.jpg" />
+            <img class="blackline" src="../img/blackline.png" />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+            <button class="detailaccueil">Détails</button>
+          </div>
+          <div class="pop2">
+            <img class="imgpopular" src="../img/PhotoPopular.jpg" />
+            <img class="blackline" src="../img/blackline.png" />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+            <button class="detailaccueil">Détails</button>
+          </div>
+          <div class="pop3">
+            <img class="imgpopular" src="../img/PhotoPopular.jpg" />
+            <img class="blackline" src="../img/blackline.png" />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+            <button class="detailaccueil">Détails</button>
+          </div>
+          <div class="pop4">
+            <img class="imgpopular" src="../img/PhotoPopular.jpg" />
+            <img class="blackline" src="../img/blackline.png" />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+            <button class="detailaccueil">Détails</button>
+          </div>
+        </div>
+      </div>
+      <div class="quatreinfoscontainer">
+        <div class="infoscontainer">
+          <img class="quatreinfos" src="../img/01.png" />
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+          </div>
+        </div>
+        <div class="infoscontainer">
+          <img class="quatreinfos" src="../img/02.png" />
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+          </div>
+        </div>
+        <div class="infoscontainer">
+          <img class="quatreinfos" src="../img/03.png" />
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+          </div>
+        </div>
+        <div class="infoscontainer">
+          <img class="quatreinfos" src="../img/04.png" />
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            expedita ad ullam quas, velit sint quibusdam nulla eos vitae
+            voluptatem neque enim accusantium maiores, id voluptate voluptates
+            suscipit delectus aliquid!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
